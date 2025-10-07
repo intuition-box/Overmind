9da2ab5d1c4c6f6f98422b17d111feaf364aecdf
Dev-Moulin <p.moulin.95@gmail.com>
2025-10-07 13:42:55 +0200

    chore: Update dependencies and draco decoder
    - Update package.json and package-lock.json
    - Add draco decoder files for GLTF compression support

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+// import App from './App.jsx'  // Old app
+import AppXState from './AppXState.tsx'  // New XState v5 app
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AppXState />
   </StrictMode>,
 )
